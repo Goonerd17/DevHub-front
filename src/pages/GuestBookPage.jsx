8a77b2dba235669395889640db0135b8ab2c2d29
Goonerd17 <tlghks1217@naver.com>
2025-11-11 23:09:18 +0900

    [test] 빌드 테스트

--- src/pages/GuestBookPage.jsx
+++ src/pages/GuestBookPage.jsx
@@ -60,7 +60,7 @@ const GuestBookPage = () => {
   return (
     <PageLayout>
       <div className="tool-container guestbook-page">
-        <h2 className="tool-title">📖 Guest Book</h2>
+        <h2 className="tool-title">📖 방명록</h2>
 
         {error && <p className="error-text">{error}</p>}
 
