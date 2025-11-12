dce1739a29b5408d02240d848881cf4cd3605ef5
Goonerd17 <tlghks1217@naver.com>
2025-11-12 22:40:50 +0900

    [test] ArgoCD test 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ const HomePage = () => {
   return (
     <PageLayout>
       <div className="tool-container home-page">
-        <h2 className="tool-title">🏠 DevHub Prototype</h2>
+        <h2 className="tool-title">🏠 DevHub Prototype v1.0</h2>
         <p>
-          DevHub Prototype에 오신 것을 환영합니다.<br />
+          DevHub에 오신 것을 환영합니다.<br />
           다양한 개발 보조 도구를 제공합니다.<br />
           상단 메뉴를 통해 원하는 기능을 이용하세요.
         </p>
