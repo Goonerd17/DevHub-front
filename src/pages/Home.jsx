efaf4e41e9f5c5947c83a99d877f94332e9f50d0
Goonerd17 <tlghks1217@naver.com>
2025-11-20 23:18:50 +0900

    [feat] test text 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,12 @@ const HomePage = () => {
   return (
     <PageLayout>
       <div className="tool-container home-page">
-        <h2 className="tool-title">🏠 DevHub Prototype v8</h2>
+        <h2 className="tool-title">🏠 DevHub Prototype</h2>
         <p>
           DevHub에 오신 것을 환영합니다.<br />
           다양한 개발 보조 도구를 제공합니다.<br />
           상단 메뉴를 통해 원하는 기능을 이용하세요.
+          ~2025-11-20 테스트 문구 추가~
         </p>
       </div>
     </PageLayout>
